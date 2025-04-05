14595d2748f0e8f3417ccf16679bc9c68d3b2470
Almario Miano <almariomiano@gmail.com>
2025-04-05 17:06:16 +0800

    added route guard for user authentication

--- music-vuejs-app/src/router/index.ts
+++ music-vuejs-app/src/router/index.ts
@@ -1,10 +1,34 @@
 import { createRouter, createWebHistory, type Router } from "vue-router";
 import routes from "./routes";
+import { useAuthStore } from "@/composables/stores/authStore";
 
 const router: Router = createRouter({
   scrollBehavior: () => ({ top: 0, left: 0, behavior: "smooth" }),
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore();
+  const isAuthenticated = authStore.isLoggedIn;
+  const hasToken = !!authStore.getToken;
+
+  // after page reload/refresh
+  if (hasToken && !isAuthenticated) {
+    await authStore.refresh();
+  }
+
+  // accessing auth pages but not signed in
+  if (!isAuthenticated && to.meta.requireAuth) {
+    return next({ name: "Guest" });
+  }
+
+  // accessing guest-only pages while currently signed in
+  if (to.meta.guestOnly && (isAuthenticated || hasToken)) {
+    return next({ name: "Dashboard" });
+  }
+
+  next();
 });
 
 export default router;
--- music-vuejs-app/src/router/routes.ts
+++ music-vuejs-app/src/router/routes.ts
@@ -3,10 +3,13 @@ import type { RouteRecordRaw } from "vue-router";
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
+    name: "Guest",
     component: () => import("../layouts/GuestLayout.vue"),
+    redirect: { name: "Login" },
+    meta: { guestOnly: true },
     children: [
       {
-        path: "",
+        path: "login",
         name: "Login",
         component: () => import("../views/auth/LoginPage.vue"),
       },
@@ -14,6 +17,21 @@ const routes: RouteRecordRaw[] = [
         path: "create-account",
         name: "Sign Up",
         component: () => import("../views/auth/SignUpPage.vue"),
+      },
+    ],
+  },
+
+  // auth
+  {
+    path: "/",
+    component: () => import("../layouts/MainLayout.vue"),
+    meta: { requireAuth: true },
+    redirect: { name: "Dashboard" },
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("../views/DashboardPage.vue"),
       },
     ],
   },
