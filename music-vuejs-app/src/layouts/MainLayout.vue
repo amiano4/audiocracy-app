5d5d96c1b1fbff4e55f5623d6c37b20c8e79e809
Almario Miano <almariomiano@gmail.com>
2025-04-05 17:26:57 +0800

    Create MainLayout.vue

---
+++ music-vuejs-app/src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="full-container">
+    <header class="flex items-center">
+      <Logo size="3rem" />
+      <FormControl placeholder="Search..." v-model="search" />
+    </header>
+    <main class="bg-danger"></main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { FormControl, Logo } from "@/components/ui";
+import { ref } from "vue";
+
+const search = ref("");
+</script>
+
+<style lang="scss">
+main {
+  flex-grow: 1;
+}
+
+header {
+  padding: 1rem;
+  gap: 1rem;
+}
+</style>
