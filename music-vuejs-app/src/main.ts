58f4bff85ec2e98361353ca6a71058d2cac8003a
Almario Miano <almariomiano@gmail.com>
2025-04-05 17:26:02 +0800

    Pinia state management setup with persisted-state

--- music-vuejs-app/src/main.ts
+++ music-vuejs-app/src/main.ts
@@ -5,9 +5,16 @@ import router from "./router/index.ts";
 
 import App from "./App.vue";
 import { createPinia } from "pinia";
+import { createPersistedState } from "pinia-plugin-persistedstate";
 
 const pinia = createPinia();
 const app = createApp(App);
+
+pinia.use(
+  createPersistedState({
+    key: (id: string) => "audiocracy_app_" + id,
+  })
+);
 
 app
   .use(router) // vue-router
